ae71f6016ec663d5653a1fc32881387488dbda24
Marina <marinaysn@gmail.com>
2019-05-22 17:14:57 -0400

    added sorting with nulls case

--- mmw1/mmw1-20.sql
+++ mmw1/mmw1-20.sql
@@ -1,10 +1,38 @@
--- 1-20. Sorting Nulls High or Low
+-- 1-20. Sorting Nulls High or Low and sorting with non existing value
 
 --ex:
 
+SELECT ProductID, Name, Weight
+FROM Production.Product
+ORDER BY ISNULL(Weight, 1) DESC, Weight;
 
+ --or 
+
+ SELECT CATEGORY, ProdCode, DescLang2
+FROM inventory
+ORDER BY ISNULL(CATEGORY, 1) desc, ProdCode;
 
 ---------------
 
+Select distinct (case when CATEGORY is null then 0 else 1 end) as 'aaa', CATEGORY
+ From  inventory
+ Order By (case when CATEGORY is null then 0 else 1 end) ,CATEGORY
 
+ --or 
 
+ Select distinct (case when CATEGORY is null then 0 else 1 end) as 'aaa', CATEGORY
+ From  inventory
+ Order By 1, 2
+---------------
+
+Select distinct CATEGORY
+ From  inventory  where CATEGORY is not null
+ union (select '{all}')
+
+-- or 
+
+Select  CATEGORY
+ From  inventory  where CATEGORY is not null
+ union (select '{all}')
+
+---------------

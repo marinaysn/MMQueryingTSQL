bcbc6886333f40d8e75e27d632878834ceeb6890
Marina <marinaysn@gmail.com>
2019-05-22 12:30:43 -0400

    added week 1 material. from information_schema.tables

---
+++ mmw1/mmw1-7.sql
@@ -0,0 +1,19 @@
+-- 1-7. Listing the Available Tables
+--will show all tables and views
+SELECT table_name, table_type
+FROM information_schema.tables
+
+---------------
+
+SELECT table_name, table_type,table_schema
+FROM information_schema.tables
+WHERE table_name like 'Inventory%';
+
+---------------
+--ex: 
+SELECT 'Select * from ' + table_schema + '.[' + table_name + '];'
+FROM information_schema.tables
+WHERE table_name like 'Inventory%'
+AND table_type = 'BASE TABLE'
+
+---------------

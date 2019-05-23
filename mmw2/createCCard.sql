48cdf68e336ec9818d0036d6f9439679e71564d2
Marina <marinaysn@gmail.com>
2019-05-23 12:46:30 -0400

    adding 2 scripts to works with mmw2 sectioin

---
+++ mmw2/createCCard.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[CreditCards](
+	[CreditCardType] [nvarchar](25) NULL,
+	[AutoID] [int] IDENTITY(1,1) NOT NULL,
+	[LastUpdatedTimestamp] [timestamp] NULL,
+	[Active] [bit] NOT NULL,
+ CONSTRAINT [CCards_PK] PRIMARY KEY NONCLUSTERED 
+(
+	[AutoID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
---
+++ mmw2/createColorTable.sql
@@ -0,0 +1,23 @@
+/* Drop constraint and table to allow for the script to be rerun at will. */
+ALTER TABLE Production.Product
+DROP CONSTRAINT FK_Product_Color;
+GO
+DROP TABLE Production.ProdColor;
+GO
+CREATE TABLE Production.ProdColor
+(
+    Color NVARCHAR(15),
+    CONSTRAINT PK_ProdColor_Color
+PRIMARY KEY (Color)
+);
+
+INSERT INTO Production.ProdColor
+    (Color)
+SELECT DISTINCT Color
+FROM Production.Product
+WHERE Color IS NOT NULL;
+GO
+ALTER TABLE Production.Product ADD
+CONSTRAINT FK_Product_Color
+FOREIGN KEY (Color)
+REFERENCES Production.ProdColor;

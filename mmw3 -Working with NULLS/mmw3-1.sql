27f60ebd09c1ad368e3b6efe0d1326adbea0e8d1
Marina <marinaysn@gmail.com>
2019-05-25 07:45:43 -0400

    added folder mmw3 to work with NULLs

---
+++ mmw3 -Working with NULLS/mmw3-1.sql
@@ -0,0 +1,43 @@
+--2-14. Looping through Query Results a Row at a Time
+
+-----------------------------
+
+
+-- Do not show rowcounts in the results
+SET NOCOUNT ON;
+DECLARE @session_id smallint;
+
+-- Declare the cursor
+DECLARE session_cursor CURSOR FORWARD_ONLY READ_ONLY FOR
+    SELECT session_id
+FROM sys.dm_exec_requests
+WHERE status IN ('runnable', 'sleeping', 'running');
+
+-- Open the cursor
+OPEN session_cursor;
+
+-- Retrieve one row at a time from the cursor
+FETCH NEXT
+FROM session_cursor
+INTO @session_id;
+
+-- Process and retrieve new rows until no more are available
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    PRINT 'Spid #: ' + STR(@session_id);
+    EXEC ('sp_who ' + @session_id);
+
+    FETCH NEXT
+        FROM session_cursor
+        INTO @session_id;
+END;
+
+-- Close the cursor
+CLOSE session_cursor;
+
+-- Deallocate the cursor
+DEALLOCATE session_cursor;
+
+
+
+

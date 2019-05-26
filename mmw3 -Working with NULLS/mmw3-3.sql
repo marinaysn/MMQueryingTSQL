9db76e6fa0473208a34082b77d672ae56062cebd
Marina <marinaysn@gmail.com>
2019-05-26 07:58:56 -0400

    add additional 3-3. Choosing Between ISNULL and COALESCE in a SELECT Statement cases

--- mmw3 -Working with NULLS/mmw3-3.sql
+++ mmw3 -Working with NULLS/mmw3-3.sql
@@ -17,11 +17,39 @@ SELECT column_ordinal,
 FROM master.sys.dm_exec_describe_first_result_set(@sql, NULL, 0) a;
 
 select *
-FROM master.sys.dm_exec_describe_first_result_set(@sql, NULL, 0) a ;
+FROM master.sys.dm_exec_describe_first_result_set(@sql, NULL, 0) a
+;
 
 -----------------------------
 
+SELECT COALESCE('five', 5)
+;
+SELECT COALESCE('five', '5')
+;
+SELECT isnull('five', 5)
+;
 
+DECLARE @i INT = NULL
+;
+SELECT ISNULL(@i, 'five')
+;
+
+-----------------------------
+
+DECLARE @sql NVARCHAR(MAX) = '
+	SELECT TOP 10 FirstName, LastName,
+	MiddleName_ISNULL = ISNULL(MiddleName, ''[none]''),
+	MiddleName_COALESCE = COALESCE(MiddleName, ''[none]'')
+	FROM Person.Person ;
+	';
+EXEC sp_executesql @sql;
+
+SELECT column_ordinal,
+    [name],
+    is_nullable
+FROM master.sys.dm_exec_describe_first_result_set(@sql, NULL, 0) a ;
+
+-----------------------------
 
 -----------------------------
 

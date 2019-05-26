ccd2587004e80b2bb29d94045ed4ae0960248b13
Marina <marinaysn@gmail.com>
2019-05-26 15:10:06 -0400

    add date and time function

---
+++ datetimeFunc.sql
@@ -0,0 +1,32 @@
+----Today
+SELECT GETDATE() 'Today'
+----Yesterday
+SELECT DATEADD(d,-1,GETDATE()) 'Yesterday'
+----First Day of Current Week
+SELECT DATEADD(wk,DATEDIFF(wk,0,GETDATE()),0) 'First Day of Current Week'
+----Last Day of Current Week
+SELECT DATEADD(wk,DATEDIFF(wk,0,GETDATE()),6) 'Last Day of Current Week'
+----First Day of Last Week
+SELECT DATEADD(wk,DATEDIFF(wk,7,GETDATE()),0) 'First Day of Last Week'
+----Last Day of Last Week
+SELECT DATEADD(wk,DATEDIFF(wk,7,GETDATE()),6) 'Last Day of Last Week'
+----First Day of Current Month
+SELECT DATEADD(mm,DATEDIFF(mm,0,GETDATE()),0) 'First Day of Current Month'
+----Last Day of Current Month
+SELECT DATEADD(ms,- 3,DATEADD(mm,0,DATEADD(mm,DATEDIFF(mm,0,GETDATE())+1,0))) 'Last Day of Current Month'
+----First Day of Last Month
+SELECT DATEADD(mm,-1,DATEADD(mm,DATEDIFF(mm,0,GETDATE()),0)) 'First Day of Last Month'
+----Last Day of Last Month
+SELECT DATEADD(ms,-3,DATEADD(mm,0,DATEADD(mm,DATEDIFF(mm,0,GETDATE()),0))) 'Last Day of Last Month'
+----First Day of Current Year
+SELECT DATEADD(yy,DATEDIFF(yy,0,GETDATE()),0) 'First Day of Current Year'
+----Last Day of Current Year
+SELECT DATEADD(ms,-3,DATEADD(yy,0,DATEADD(yy,DATEDIFF(yy,0,GETDATE())+1,0))) 'Last Day of Current Year'
+----First Day of Last Year
+SELECT DATEADD(yy,-1,DATEADD(yy,DATEDIFF(yy,0,GETDATE()),0)) 'First Day of Last Year'
+----Last Day of Last Year
+SELECT DATEADD(ms,-3,DATEADD(yy,0,DATEADD(yy,DATEDIFF(yy,0,GETDATE()),0))) 'Last Day of Last Year'
+----Next Year
+SELECT YEAR(GETDATE()) + 1 as 'Next Year', MONTH(GETDATE()) 'Month', DAY(GETDATE()) 'Day';
+----Next Year April 30
+SELECT convert(CHAR(4), year(sysdatetime()) + 1) + '0430' as 'NextYearApril30';

587c8749206656169162efce4266478a06aeb528
Marina <marinaysn@gmail.com>
2019-05-28 23:32:02 -0400

    added example of query with multiple tables

--- mmw4 - Querying from Multiple Tables/mmw4-2.sql
+++ mmw4 - Querying from Multiple Tables/mmw4-2.sql
@@ -2,11 +2,20 @@
 
 -----------------------------
 
+SELECT p.Name,
+    s.DiscountPct
+FROM Sales.SpecialOffer s
+    INNER JOIN Sales.SpecialOfferProduct o
+    ON s.SpecialOfferID = o.SpecialOfferID
+    INNER JOIN Production.Product p
+    ON o.ProductID = p.ProductID
+WHERE p.Name = 'All-Purpose Bike Stand';
 
+----------------------------------------- Present the data
 
------------------------------
-
-
+SELECT * from Sales.SpecialOffer WHERE SpecialOfferID   = 1;
+SELECT * from Sales.SpecialOfferProduct WHERE ProductID  = 879;
+SELECT * from Production.Product WHERE Name = 'All-Purpose Bike Stand';
 
 -----------------------------
 

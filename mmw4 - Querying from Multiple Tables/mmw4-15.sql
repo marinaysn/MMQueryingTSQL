11a57b66f5aca517fcfd0526e1b028f4b3117c66
Marina <marinaysn@gmail.com>
2019-06-06 19:47:59 -0400

    add 4-15. Comparing Two Tables

--- mmw4 - Querying from Multiple Tables/mmw4-15.sql
+++ mmw4 - Querying from Multiple Tables/mmw4-15.sql
@@ -1,24 +1,64 @@
 ----4-15. Comparing Two Tables
 
 -----------------------------
-
+    SELECT *,
+        COUNT(*) DupeCount,
+        'Password' TableName
+    FROM Person.Password P
+    GROUP BY BusinessEntityID,
+PasswordHash,
+PasswordSalt,
+rowguid,
+ModifiedDate
+    HAVING NOT EXISTS ( SELECT *,
+        COUNT(*)
+    FROM Person.PasswordCopy PC
+    GROUP BY BusinessEntityID,
+PasswordHash,
+PasswordSalt,
+rowguid,
+ModifiedDate
+    HAVING PC.BusinessEntityID = P.BusinessEntityID
+        AND PC.PasswordHash = P.PasswordHash
+        AND PC.PasswordSalt = P.PasswordSalt
+        AND PC.rowguid = P.rowguid
+        AND PC.ModifiedDate = P.ModifiedDate
+        AND COUNT(*) = COUNT(ALL P.BusinessEntityID) )
+UNION
+    SELECT *,
+        COUNT(*) DupeCount,
+        'PasswordCopy' TableName
+    FROM Person.PasswordCopy PC
+    GROUP BY BusinessEntityID,
+PasswordHash,
+PasswordSalt,
+rowguid,
+ModifiedDate
+    HAVING NOT EXISTS ( SELECT *,
+        COUNT(*)
+    FROM Person.Password P
+    GROUP BY BusinessEntityID,
+PasswordHash,
+PasswordSalt,
+rowguid,
+ModifiedDate
+    HAVING PC.BusinessEntityID = P.BusinessEntityID
+        AND PC.PasswordHash = P.PasswordHash
+        AND PC.PasswordSalt = P.PasswordSalt
+        AND PC.rowguid = P.rowguid
+        AND PC.ModifiedDate = P.ModifiedDate
+        AND COUNT(*) = COUNT(ALL PC.BusinessEntityID) );
 
 
 ----------------------------------------- Present the data
 
-
+SELECT *
+INTO Person.PasswordCopy
+FROM Person.Password;
 -----------------------------
 
     
------------------------------
 
-
-
-
------------------------------
-
-    
------------------------------
 
 
 

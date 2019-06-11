62ff5bda66562f2b650f805215867dad091c8e4d
Marina <marinaysn@gmail.com>
2019-06-10 22:31:39 -0400

    Creating Aggregations Based upon the Values of the Data

--- mmw5 - Aggregations and Grouping/mmw5-2.sql
+++ mmw5 - Aggregations and Grouping/mmw5-2.sql
@@ -2,13 +2,31 @@
 
 -----------------------------
 
-
+SELECT TOP (10)
+    SalesOrderID,
+    SUM(LineTotal) AS OrderTotal,
+    MIN(LineTotal) AS MinLine,
+    MAX(LineTotal) AS MaxLine,
+    AVG(LineTotal) AS AvgLine,
+    COUNT(LineTotal) AS CountLine
+FROM [Sales].[SalesOrderDetail]
+GROUP BY SalesOrderID
+ORDER BY SalesOrderID;
 
 ----------------------------------------- Present the data
-
+Select  * from [Sales].[SalesOrderDetail] --where SalesOrderID = 43659
 
 -----------------------------
-
+SELECT 
+   
+    SUM(LineTotal) AS OrderTotal,
+    MIN(LineTotal) AS MinLine,
+    MAX(LineTotal) AS MaxLine,
+    AVG(LineTotal) AS AvgLine,
+    COUNT(LineTotal) AS CountLine
+FROM [Sales].[SalesOrderDetail]
+GROUP BY SalesOrderID
+ORDER BY SalesOrderID;
     
 -----------------------------
 

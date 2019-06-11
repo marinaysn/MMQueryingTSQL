ec4c2fbfa94d2b6af5688fda0be660cb072be6b4
Marina <marinaysn@gmail.com>
2019-06-10 22:20:00 -0400

    intro to aggregation functions

--- mmw5 - Aggregations and Grouping/mmw5-1.sql
+++ mmw5 - Aggregations and Grouping/mmw5-1.sql
@@ -2,13 +2,26 @@
 
 -----------------------------
 
-
+SELECT MIN(Rating) Rating_Min,
+    MAX(Rating) Rating_Max,
+    SUM(Rating) Rating_Sum,
+    AVG(Rating) Rating_Avg
+FROM Production.ProductReview;
 
 ----------------------------------------- Present the data
 
 
 -----------------------------
-
+SELECT AVG(Grade) AS AvgGrade,
+    AVG(DISTINCT Grade) AS AvgDistinctGrade
+FROM (VALUES
+        (1, 100),
+        (1, 100),
+        (1, 100),
+        (1, 100),
+        (1, 100),
+        (1, 30)
+) dt (StudentId, Grade);
     
 -----------------------------
 
